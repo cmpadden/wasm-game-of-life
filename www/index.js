4b332309cf6217844fb8a99fb0ee6a47e3faa1c8
Colton Padden <colton.padden@fastmail.com>
2022-03-24 19:47:22 -0400

    4.8. Time Profiling (For the most part...)

--- www/index.js
+++ www/index.js
@@ -4,6 +4,7 @@ import { Universe } from "wasm-game-of-life";
 //                                                Constants and Globals                                                //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const SIZE = 64;
 let animationFrame = null;
 let framesPerAnimation = 30; // 30 frames will go by before we animate
 
@@ -37,6 +38,7 @@ const draw = () => {
 };
 
 const renderLoop = () => {
+  fps.render();
   if (animationFrame % (61 - framesPerAnimation) === 0) {
     draw();
     universe.tick();
@@ -66,7 +68,7 @@ inputSpeed.addEventListener("change", (_event) => {
 });
 
 pre.addEventListener("click", (event) => {
-  const squareSize = pre.clientWidth / 64;
+  const squareSize = pre.clientWidth / SIZE;
   const col = Math.floor(event.offsetX / squareSize);
   const row = Math.floor(event.offsetY / squareSize);
   universe.toggle_cell(row, col);
@@ -74,11 +76,54 @@ pre.addEventListener("click", (event) => {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                         FPS                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+fps: ${Math.round(fps)}
+avg: ${Math.round(mean)}
+min: ${Math.round(min)}
+max: ${Math.round(max)}
+`.trim();
+  }
+})();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                     Entrypoint                                                      //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const universe = Universe.new();
-universe.set_height = 64;
-universe.set_width = 64;
+const universe = Universe.new(100, 100);
 
 draw();
